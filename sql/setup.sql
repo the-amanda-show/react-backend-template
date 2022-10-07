37042877b99d2784ef3f96f48f579f99efc905db
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-10-07 16:44:30 -0600

    test wrote

--- sql/setup.sql
+++ sql/setup.sql
@@ -15,6 +15,7 @@ CREATE TABLE users (
 CREATE TABLE todos (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   user_id BIGINT,
+  qty BIGINT,
   todo TEXT,
   location TEXT,
   completed BOOLEAN NOT NULL DEFAULT(FALSE),
@@ -28,17 +29,17 @@ INSERT into users (first_name, last_name, email, password_hash) values
 ('Selina','Kyle','meowbitch@leather.com','ilovecats'),
 ('Harley','Quinn','ilovejoker@crazytrain.com','jokerjoker');
 
-INSERT into todos (user_id, location, todo, description, completed) values
-('1','police station','Interigate Penguin','DID HE KILL MY PARENTS?!?!?!?!?!','false'),
-('2','batcave','clean batcave','Clean up bat poop from ALL the stuff they poop on','false'),
-('3','home','feed cats','Make sure my sweeties are nice and fed','false'),
-('4','office','help Riddler','Find out what he did *FOR JOKER*','false'),
-('1','arkam asylum','drop off Riddler','Leave Riddler for the damage he caused','false'),
-('2','batcave','clean vehicles','Need to get all the junkfood and blood from inside the vehicle, and spray off the outside','false'),
-('3','jewlrey store','steal jewlrey','need new jewley to match my babies collars','false'),
-('4','office','help Scarecrow','find out what Scarecrow keeps his gas *FOR JOKER*','false'),
-('1','bruce manor','do Bruce Wanye things','sign paperwork, send emails, kiss babies','false'),
-('2','batcave','feed bats','bats gotta eat','false'),
-('3','jewlrey store','steal gems','need gems for new kitty collars','false'),
-('4','arkam asylum','set JOKER free','break out JOKER after I get all the info he needs!','false')
+INSERT into todos (user_id, qty, location, todo, description, completed) values
+('1', '1', 'police station','Interigate Penguin','DID HE KILL MY PARENTS?!?!?!?!?!','false'),
+('2','1','batcave','clean batcave','Clean up bat poop from ALL the stuff they poop on','false'),
+('3','1','home','feed cats','Make sure my sweeties are nice and fed','false'),
+('4','1','office','help Riddler','Find out what he did *FOR JOKER*','false'),
+('1','1','arkam asylum','drop off Riddler','Leave Riddler for the damage he caused','false'),
+('2','1','batcave','clean vehicles','Need to get all the junkfood and blood from inside the vehicle, and spray off the outside','false'),
+('3','1','jewlrey store','steal jewlrey','need new jewley to match my babies collars','false'),
+('4','1','office','help Scarecrow','find out what Scarecrow keeps his gas *FOR JOKER*','false'),
+('1','1','bruce manor','do Bruce Wanye things','sign paperwork, send emails, kiss babies','false'),
+('2','1','batcave','feed bats','bats gotta eat','false'),
+('3','1','jewlrey store','steal gems','need gems for new kitty collars','false'),
+('4','1','arkam asylum','set JOKER free','break out JOKER after I get all the info he needs!','false')
 
